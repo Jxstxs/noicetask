0a392ef1db9254fa32c5e676865c20e34e884508
Jxstxs <juleb0202@gmail.com>
2022-06-19 23:50:33 +0200

    Macro which checks if the next element is usable and safes it into the given variable. if its not usable it sets the given error and returns the current state of the todo config

--- src/argparse.h
+++ src/argparse.h
@@ -1,6 +1,7 @@
 #ifndef ARGPARSE_H
 
 #define ARGCMP(x, y, z) ((strcmp(argv[i], x) == 0 || strcmp(argv[i], y) == 0) && _tc.func == z)
+#define CHECKNEXT(x, y) if (argv[i+1]) { _tc.fp.x = argv[i+1]; skip = 1; } else { _tc.err = y; return _tc; }
 
 todoConf parseArguments(int argc, char** argv);
 
