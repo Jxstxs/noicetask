b8e498a63cb192bb1ac44d8f7af2b91f04d844b0
Jxstxs <juleb0202@gmail.com>
2022-06-19 23:49:19 +0200

    added -db and -h support, also function detection, add function parameter and done parameters

--- src/argparse.c
+++ src/argparse.c
@@ -23,37 +23,48 @@ todoConf parseArguments(int argc, char** argv){
             i = i + skip;
             skip = 0;
         }
-
         if (!argv[i]) break;
 
         // ARGUMENTS
         // DATABASE ARG
-//      if (ARGCMP("-db", "--database") && _tc.func == NONE) {
-//          // checks if database is given
-//          if (argv[i+1]) {
-//              _tc.databasePath = argv[i+1];
-//              skip = 1;
-//          } else {
-//              _tc.err = NO_DB_PATH;
-//              return _tc;
-//          }
-//      }
-//      // HELP ARG
-//      else if (ARGCMP("-h", "--help") && _tc.func == NONE) {
-//          _tc.err = HELP;
-//          return _tc;
-//      }
+        if (ARGCMP("-db", "--database", NONE)){
+            // checks if database is given
+            if (argv[i+1]) {
+                _tc.databasePath = argv[i+1];
+                skip = 1;
+            } else {
+                _tc.err = NO_DB_PATH;
+                return _tc;
+            }
+        }
+        // HELP ARG
+        else if (ARGCMP("-h", "--help", NONE)) {
+            _tc.err = HELP;
+            return _tc;
+        }
 //      // CREATE ARG
 //      else if (ARGCMP("-cr", "--create") && _tc.func == NONE) {
 //          // HERE 
 //      }
 
         // FUNCTIONS
-//      else if (ARGCMP("a", "add") && _tc.func == NONE) _tc.func = ADD;
-//      else if (ARGCMP("d", "done") && _tc.func == NONE) _tc.func = DONE;
-//      else if (ARGCMP("r", "remove") && _tc.func == NONE) _tc.func = REMOVE;
-//      else if (ARGCMP("m", "modify") && _tc.func == NONE) _tc.func = MOD;
-//      else if (ARGCMP("p", "print") && _tc.func == NONE) _tc.func = PRINT;
+        else if (ARGCMP("a", "add", NONE)) _tc.func = ADD_TODO;
+        else if (ARGCMP("d", "done", NONE)) _tc.func = DONE_TODO;
+        else if (ARGCMP("r", "remove", NONE)) _tc.func = REMOVE_TODO;
+        else if (ARGCMP("m", "modify", NONE)) _tc.func = MOD_TODO;
+        else if (ARGCMP("p", "print", NONE)) _tc.func = LIST;
+        
+        // FUNCTION PARAMETERS
+        // ADD
+        else if (ARGCMP("-t", "--title", ADD_TODO))         {CHECKNEXT(title, PARAM)}
+        else if (ARGCMP("-d", "--description", ADD_TODO))   {CHECKNEXT(description, PARAM)}
+        else if (ARGCMP("-pa", "--path", ADD_TODO))         {CHECKNEXT(title, PARAM)}
+        else if (ARGCMP("-ex", "--expire", ADD_TODO))       {CHECKNEXT(title, PARAM)}
+        else if (ARGCMP("-ca", "--category", ADD_TODO))     {CHECKNEXT(title, PARAM)}
+        else if (ARGCMP("-pr", "--priority", ADD_TODO))     {CHECKNEXT(title, PARAM)}
+
+        // DONE
+        else if (ARGCMP("-id", "-id", DONE_TODO)) {CHECKNEXT(ids, PARAM)}
     }
 
     if (_tc.func == NONE) _tc.err = NO_FUNC_GIVEN;
