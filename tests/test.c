60f06ef11f5932cd69b9f08c64bfa720a4aaae52
Jxstxs <juleb0202@gmail.com>
2022-06-19 16:07:24 +0200

    test file

---
+++ tests/test.c
@@ -0,0 +1,31 @@
+
+#include  <stdio.h>
+#include  <stdlib.h>
+#include  <time.h>
+    
+int main(){
+    
+    // TIME STUFF
+//  time_t timestamp;
+//  struct tm* timeinfo;
+//  time(&timestamp);
+//  timeinfo = localtime(&timestamp);
+//  printf("%d %d\n", timestamp, timeinfo->tm_sec);
+
+    // ENV STUFF
+//  char *HOMEDIR = getenv("DOT");
+//  if (HOMEDIR) printf("%s\n", HOMEDIR);
+
+    // SYSTEM STUFF
+    FILE *out;
+    char buf[1024];
+    out = popen("echo 1\n2\n3 | fzf -m --prompt 'Choose Id: ' --preview 'echo Hallo\nDave'", "r");
+    if (!out) {
+        printf("error executing\n");
+        return 1;
+    }
+    while (fgets(buf, 1024, out)) {
+        printf("%s", buf);
+    }
+    return 0;
+}
