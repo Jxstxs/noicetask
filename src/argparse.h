195ce61d772cd55e3b7f21bedd22cc6a97217634
Jxstxs <juleb0202@gmail.com>
2022-06-22 00:03:51 +0200

    any support and right include guard

--- src/argparse.h
+++ src/argparse.h
@@ -1,6 +1,9 @@
 #ifndef ARGPARSE_H
+#define ARGPARSE_H
 
-#define ARGCMP(x, y, z) ((strcmp(argv[i], x) == 0 || strcmp(argv[i], y) == 0) && _tc.func == z)
+#include "main.h"
+
+#define ARGCMP(x, y, z) ((strcmp(argv[i], x) == 0 || strcmp(argv[i], y) == 0) && (_tc.func == z || z == ANY))
 #define CHECKNEXT(x, y) if (argv[i+1]) { _tc.fp.x = argv[i+1]; skip = 1; } else { _tc.err = y; return _tc; }
 
 todoConf parseArguments(int argc, char** argv);
