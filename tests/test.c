1c5534e9320d666f27d01703f709efc4090d8022
Jxstxs <juleb0202@gmail.com>
2022-06-22 12:41:58 +0200

    commentet out

--- tests/test.c
+++ tests/test.c
@@ -17,15 +17,15 @@ int main(){
 //  if (HOMEDIR) printf("%s\n", HOMEDIR);
 
     // SYSTEM STUFF
-    FILE *out;
-    char buf[1024];
-    out = popen("echo 1\n2\n3 | fzf -m --prompt 'Choose Id: ' --preview 'echo Hallo\nDave'", "r");
-    if (!out) {
-        printf("error executing\n");
-        return 1;
-    }
-    while (fgets(buf, 1024, out)) {
-        printf("%s", buf);
-    }
+//  FILE *out;
+//  char buf[1024];
+//  out = popen("echo 1\n2\n3 | fzf -m --prompt 'Choose Id: ' --preview 'echo Hallo\nDave'", "r");
+//  if (!out) {
+//      printf("error executing\n");
+//      return 1;
+//  }
+//  while (fgets(buf, 1024, out)) {
+//      printf("%s", buf);
+//  }
     return 0;
 }
