5005a3f407d55edab7f1c436334be0f05f8d3acb
Jxstxs <juleb0202@gmail.com>
2022-06-28 12:36:37 +0200

    now usefull tests without useless crap

--- tests/additional-tests.c
+++ tests/additional-tests.c
@@ -1,39 +1,31 @@
 
+#include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include <stdio.h>
 
-#include "printConf.h"
-
-#include "../src/main.h"
-#include "../src/argparse.h"
 #include "../src/additional.h"
 
 int main (int argc, char *argv[])
 {
-    todoConf tc;
-    initTodoConf(&tc);
-
-    // setting up arguments
-    int argc_ = 12;
-    char *argv_[] = {
-        "./todo",
-        "add",
-        "-t", "test",
-        "-ex", "2020/01/01/00/00",
-        "-ca", "1,2,3",
-        "-pr", "1",
-        "-pa", "test.txt"
-    };
-
-    // parsing arguments
-    tc = parseArguments(argc_, argv_);
-
-    // printing arguments
-    printTodoConfig(tc);
 
     // parse date
-    time_t tm = convertStringToDate(tc.fp.expireDate);
-    printf("%s\n", asctime(localtime(&tm)));
+    time_t tm = convertStringToDate("20220/12/1/6");
+    printf("%s", asctime(localtime(&tm)));
+
+    // parse categories
+    char *categories = "32,1234,13,4,12,67,32,432";
+    int *categoriesArr = parseCategories(categories);
+    for (int i = 0; categoriesArr[i] != 0; i++) {
+        printf("%d ", categoriesArr[i]);
+    }
+    printf("\n");
+    free(categoriesArr);
+
+    // parse priority
+    char *priority = "h";
+    int priorityInt = parsePriority(priority);
+    printf("%d\n", priorityInt);
 
     return 0;
 }
