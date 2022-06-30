d72cd6a85137230b6c3f3ee4fce660b88d18a809
Jxstxs <juleb0202@gmail.com>
2022-06-30 22:31:13 +0200

    database check loop

--- src/database.c
+++ src/database.c
@@ -8,15 +8,59 @@
 #include "database.h"
 
 void initDatabase(todoConf *tc) {
-    /*
-        NOTE: what it should do
-        - check if a database was given
-            - if yes -> try open it (check if exists and read/writeable)
-            - if no -> ask to create (if nc -> without asking)
-    */
+    int rc, i;
+    char path[512];
+    char answer[2];
 
     // check if databasePath is given
     if (strcmp(tc->databasePath, "") == 0) {
+        printf("No database path given.\n");
+        char upperPWD[512];
+        sprintf(upperPWD, "%s/..", getenv("PWD"));
+
+        char *paths[] = {
+            getenv("PWD"),
+            upperPWD,
+            getenv("HOME"),
+            NULL
+        };
+
+        // check paths for database
+        rc = -1;
+        i = 0;
+        while (paths[i] != NULL) {
+            sprintf(path, "%s/%s", paths[i], DATABASENAME);
+            if (access(path, F_OK) == 0 && access(path, R_OK) == 0 && access(path, W_OK) == 0) {
+                rc = i;
+                break;
+            }
+            i++;
+        }
+
+        // if database was found
+        if (rc == -1) {
+            // loop through paths and ask to create database
+            i = 0;
+            while (paths[i] != NULL) {
+                printf("Database not found at %s\n", paths[i]);
+                printf("Do you want to create it? (y/n) ");
+                scanf("%s", answer);
+                if (strcmp(answer, "y") == 0) {
+                    sprintf(path, "%s/%s", paths[i], DATABASENAME);
+                    printf("Creating database at %s\n", path);
+                    rc = sqlite3_open(path, &tc->db);
+                    if (rc) {
+                        printf("Can't open database: %s\n", sqlite3_errmsg(tc->db));
+                        sqlite3_close(tc->db);
+                        exit(1);
+                    }
+                    createTables(tc);
+                    break;
+                }
+                printf("\n");
+                i++;
+            }
+        }
 
     }
 }
