ca8ed9b601de6a134495be7ba5b02385864f3da0
Jxstxs <juleb0202@gmail.com>
2022-07-04 23:21:04 +0200

    some renaming

--- src/argparse.h
+++ src/argparse.h
@@ -6,6 +6,7 @@
 #define ARGCMP(x, y, z) ((strcmp(argv[i], x) == 0 || strcmp(argv[i], y) == 0) && (_tc.func == z || z == ANY))
 #define CHECKNEXT(x, y) if (argv[i+1]) { _tc.fp.x = argv[i+1]; skip = 1; } else { _tc.err = y; return _tc; }
 
-todoConf parseArguments(int argc, char** argv);
+taskConf parseArguments(int argc, char** argv);
+void initTaskConf(taskConf *tc);
 
 #endif // ARGPARSE_H
